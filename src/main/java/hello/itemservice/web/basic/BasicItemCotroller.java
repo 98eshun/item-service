990a036a4e2112b9f0bd1748c5f420013aecb77b
S_H_Lee <dabansa2@naver.com>
2023-09-21 00:40:09 +0900

    RedirectAttribute 스프링 MVC 끝

--- src/main/java/hello/itemservice/web/basic/BasicItemCotroller.java
+++ src/main/java/hello/itemservice/web/basic/BasicItemCotroller.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.annotation.PostConstruct;
 import java.util.List;
@@ -94,7 +95,7 @@ public class BasicItemCotroller {
     }
 
 // String이나 Int 같은 기본형이 아니면 @ModelAttribute 생략 가능
-    @PostMapping("/add")
+//    @PostMapping("/add")
     public String addItemV4(Item item) {
 
         itemRepository.save(item);
@@ -103,6 +104,28 @@ public class BasicItemCotroller {
 
         return "basic/item";
     }
+
+// 새로 고침은 마지막에 서버에 전송한 데이터를 다시 전송한다. = redirect를 사용하는 이유
+//    @PostMapping("/add")
+    public String addItemV5(Item item) {
+
+        itemRepository.save(item);
+
+        return "redirect:/basic/items/" + item.getId(); //url은 인코딩을 해야되기 때문에 이 방식은 위험
+    }
+
+    @PostMapping("/add")
+    public String addItemV6(Item item, RedirectAttributes redirectAttributes) {
+
+        Item savedItem = itemRepository.save(item);
+
+        redirectAttributes.addAttribute("itemId", savedItem.getId());
+        redirectAttributes.addAttribute("status", true);
+
+        return "redirect:/basic/items/{itemId}";    // redirectAttributes에 넣은 itemId가 주입 된다.
+                                                    // 또한 사용되지 않은 status는 ?status 같은 쿼리 파라미터 형태로 넘어감
+    }
+
 
 // 수정 폼으로 이동
     @GetMapping("/{itemId}/edit")
